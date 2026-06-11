cdd68278ccf9f88b2476bf45962bd02e23ebe638
IDGS-803-23001508 <pabloescuela536@gmail.com>
2026-06-11 00:29:26 -0600

    Se agrega categorias

---
+++ ElZarape/Controllers/CategoriaController.cs
@@ -0,0 +1,23 @@
+﻿using ElZarape.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+namespace ElZarape.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private static List<Categoria> categorias = new();
+
+        public IActionResult Index() {
+            return View(categorias);
+        }
+
+        [HttpPost]
+
+        public IActionResult Agregar(Categoria categoria) {
+            categoria.idCategoria = categorias.Count + 1;
+            categorias.Add(categoria);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ ElZarape/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace ElZarape.Models
+{
+    public class Categoria
+    {
+        public int idCategoria { get; set; }
+
+        public string descripcion { get; set; }
+
+        public string tipo { get; set; }
+
+        public bool estatus { get; set; }
+    }
+}
