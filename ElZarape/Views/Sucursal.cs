33bf9d920c02aaf3fb1986c9ca20d83a042892da
IDGS-90323001506 <mi.hernan672@gmail.com>
2026-06-12 17:24:25 -0600

    Add files via upload

---
+++ ElZarape/Views/Sucursal.cs
@@ -0,0 +1,13 @@
+namespace ElZarape.Models
+{
+    public class Sucursal
+    {
+        public string Nombre { get; set; }
+        public string Calle { get; set; }
+        public string NumCalle { get; set; }
+        public string Colonia { get; set; }
+        public string CiudadNombre { get; set; }
+        public string EstadoNombre { get; set; }
+        public bool Activo { get; set; }
+    }
+}
